97a65127fafe07f0c914aaef65fafb88b028398f
Matthias Grundmann <matthias@glasmail.de>
2015-10-25 22:30:50 +0100

    Scope routes because relative url doesn't work for links

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,39 @@
 Tracklog::Application.routes.draw do
-  root to: "dashboard#index", as: :dashboard
+  scope 'tracklog' do
+    root to: "dashboard#index", as: :dashboard
 
-  match "login" => "auth#login", as: :login, via: [:get, :post]
-  put "logout" => "auth#logout", as: :logout
+    match "login" => "auth#login", as: :login, via: [:get, :post]
+    put "logout" => "auth#logout", as: :logout
 
-  get "profile" => "profile#index",  as: :profile
-  patch "profile" => "profile#update", as: :update_profile
+    get "profile" => "profile#index",  as: :profile
+    patch "profile" => "profile#update", as: :update_profile
 
-  get "dashboard/activity_plots_data" => "dashboard#activity_plots_data"
+    get "dashboard/activity_plots_data" => "dashboard#activity_plots_data"
 
-  get "admin" => "admin#index", as: :admin
+    get "admin" => "admin#index", as: :admin
 
-  resources :logs do
-    collection do
-      get "year/:year" => :index, as: :year
-      get "tag/:tag" => :tagged, as: :tagged
-    end
-
-    member do
-      get :tracks
-    end
-
-    resources :tracks do
-      resources :trackpoints
+    resources :logs do
+      collection do
+        get "year/:year" => :index, as: :year
+        get "tag/:tag" => :tagged, as: :tagged
+      end
 
       member do
-        match :transfer, via: [:get, :post]
-        put "split/:trackpoint_id" => :split, as: :split
+        get :tracks
+      end
+
+      resources :tracks do
+        resources :trackpoints
+
+        member do
+          match :transfer, via: [:get, :post]
+          put "split/:trackpoint_id" => :split, as: :split
+        end
       end
     end
-  end
 
-  namespace :admin do
-    resources :users
+    namespace :admin do
+      resources :users
+    end
   end
 end
