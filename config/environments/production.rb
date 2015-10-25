10bfd855b5bb4d185160e20ad7020f5ba9a44c21
Matthias Grundmann <matthias@glasmail.de>
2015-10-25 22:05:53 +0100

    Configure relative url root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Tracklog::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.relative_url_root = '/tracklog'
 end
